c3872f922f539a6546913679faeb7a37a00cc4bd
TrissNepps <74569212+TrissNepps@users.noreply.github.com>
2021-01-24 01:57:59 -0500

    Create .js

---
+++ .js
@@ -0,0 +1,49 @@
+const temperatures = [76, 67, 34, 2, 64, 65, "error", 65];
+const temperatures2 = [70, 60, 78, 5, 64, 65, "error", 65];
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) {
+      max = curTemp;
+    }
+    if (curTemp < min) {
+      min = curTemp;
+    }
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
+
+//Now make function take 2 arrays of temps - merge 2 arrays
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) {
+      max = curTemp;
+    }
+    if (curTemp < min) {
+      min = curTemp;
+    }
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTempAmplitudeNew(temperatures, temperatures2);
+console.log(amplitude);
